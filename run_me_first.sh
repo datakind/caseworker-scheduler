83e1a175f88c45fd2745df10480751794c52b873
michael dowd <mikdowd@gmail.com>
2018-09-07 16:49:17 -0400

    initial docker setup

---
+++ run_me_first.sh
@@ -0,0 +1,10 @@
+service mysql start; echo "create database cbc_schedule" | mysql;
+
+( cd cbc_api ; cbc_api add_config --overwrite  --key MYSQL_HOST --value localhost )
+( cd cbc_api ; cbc_api add_config --overwrite  --key  MYSQL_USER --value root )
+( cd cbc_api ; cbc_api add_config --overwrite  --key  MYSQL_PW --value password)
+( cd cbc_api ; cbc_api add_config --overwrite  --key MSDN_KEY --value AgzKE3GMgjpkvOlG9_N4QhBxHeUkVsfSJqHnNfi-fBdZxatkk3WGIzuwTSXDM0B3 )
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'password'" | mysql;
+( cd cbc_api ; cbc_api initialize_tables)
+
+
